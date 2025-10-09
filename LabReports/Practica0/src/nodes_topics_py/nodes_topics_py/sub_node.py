ab37bc195f1f848afced114ce0266a40ab5c8852
Roberto Miranda <99490767+Cannonball777@users.noreply.github.com>
2025-10-08 21:44:35 -0600

    Add files via upload

---
+++ LabReports/Practica0/src/nodes_topics_py/nodes_topics_py/sub_node.py
@@ -0,0 +1,34 @@
+#!usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import Int32, Float64
+
+
+
+class SubNode(Node):
+  def __init__(self ):
+    super().__init__("node_sub")
+    self.mensaje_ = 0.0
+    self.subcriber_ = self.create_subscription(Float64,"publish_topic_velrad",self.sin_publish_rpm,10)
+    self.vel_rpm_motor_ = self.create_publisher(Float64,"publish_topic_rpm",10)
+    self.get_logger().info("Nodo subcriptor activo")
+
+  def sin_publish_rpm(self,msg):
+    self.mensaje_ = msg.data
+    # self.get_logger().info("El mensaje es " + str(self.mensaje_))
+    self.new_msg_ = Float64()
+    self.new_msg_.data = self.mensaje_*9.55
+    self.vel_rpm_motor_.publish(self.new_msg_)
+    self.get_logger().info("La velocidad en rpm es: "+str(self.new_msg_.data))
+
+def main(args=None):
+  rclpy.init(args=args)
+  node = SubNode()
+  rclpy.spin(node)
+  rclpy.shutdown()
+
+if __name__ == "__main__":
+  main()
+  
